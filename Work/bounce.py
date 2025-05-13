e83d7267f27ab667eebf9aa8469e22e3f1333754
emiliakiller <emiliaalexandrakiller@gmail.com>
2025-05-13 16:48:38 +0200

    Wrote main function for bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
-#
+# A rubber ball is dropped from a height of 100 meters and each time it hits the ground, it bounces back up to 3/5 the height it fell. Write a program that prints a table showing the height of the first 10 bounces.
 # Exercise 1.5
+
+def main(starting_height, retained_height, number_of_bounces):
+    count = 0
+    current_height = starting_height
+
+    while count < number_of_bounces:
+        count += 1
+        current_height *= retained_height
+        print(f"Bounce {count}: {current_height:.4g}m ")
+
+main(100, 0.6, 10)
