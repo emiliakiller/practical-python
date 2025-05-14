181e9681fcad7da472615f09bda717714fd35b52
emiliakiller <emiliaalexandrakiller@gmail.com>
2025-05-14 15:28:46 +0200

    Initial solution and v1 of pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
-#
+# The columns in portfolio.csv correspond to the stock name, number of shares, and purchase price of a single stock holding. Write a program called pcost.py that opens this file, reads all lines, and calculates how much it cost to purchase all of the shares in the portfolio.
 # Exercise 1.27
+
+total_cost = 0
+
+with open("Work/Data/portfolio.csv", "rt") as f:
+   headers = next(f).split(',')
+   for line in f:
+       row = line.split(',')
+       total_cost += (int(row[1])*float(row[2]))
+       print(total_cost)
