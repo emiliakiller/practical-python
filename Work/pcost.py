7763433f82b8e1bcebdf3d619d564ce01d360f68
emiliakiller <emiliaalexandrakiller@gmail.com>
2025-05-14 15:44:18 +0200

    Modified pcost.py to use csv module

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,18 +2,19 @@
 # The columns in portfolio.csv correspond to the stock name, number of shares, and purchase price of a single stock holding. Write a program called pcost.py that opens this file, reads all lines, and calculates how much it cost to purchase all of the shares in the portfolio.
 # Exercise 1.27
 
+import csv
 
 def portfolio_cost(filename):
     total_cost = 0
 
     with open(filename, "rt") as f:
-       _ = next(f).split(',')
-       for line in f:
-           row = line.split(',')
+        rows = csv.reader(f)
+        _ = next(rows)
+        for row in rows:
            try:
                total_cost += (int(row[1])*float(row[2]))
            except ValueError:
-               print(f"Couldn't parse line {line}")
+               print(f"Couldn't parse line {row}")
 
     return total_cost
 
