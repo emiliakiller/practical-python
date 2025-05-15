09dd424ebae56c3b2f1d37ca085a50d6360f1e08
emiliakiller <emiliaalexandrakiller@gmail.com>
2025-05-15 13:13:32 +0200

    Modified pcost.py to take a target file argument

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,20 +3,30 @@
 # Exercise 1.27
 
 import csv
+import sys
+
+records = []
 
 def portfolio_cost(filename):
     total_cost = 0
 
     with open(filename, "rt") as f:
         rows = csv.reader(f)
-        _ = next(rows)
+        next(rows)
         for row in rows:
-           try:
-               total_cost += (int(row[1])*float(row[2]))
-           except ValueError:
-               print(f"Couldn't parse line {row}")
+
+            try:
+                records.append((row[0], int(row[1]), float(row[2])))
+                total_cost += (int(row[1])*float(row[2]))
+            except ValueError:
+                print(f"Couldn't parse line {row}")
 
     return total_cost
 
-cost = portfolio_cost('Work/Data/missing.csv')
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Work/Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
 print('Total cost:', cost)
